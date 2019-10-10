8bf3c5ac4bfe00c2d777e4280230ce08c62328e6
Brianna Jordan <bryvjordan9@gmail.com>
2019-10-10 14:01:20 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,15 @@
 class Cat
   # code goes here
+  attr_reader :owner, :mood 
+
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name 
+    @mood = "nervous"
+    @owner = owner 
+    @@all << self
+  end 
+  
+    
 end
